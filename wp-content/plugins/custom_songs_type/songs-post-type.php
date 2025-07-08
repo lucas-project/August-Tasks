98a1e21a3a123a42ad94693986601840e406e558
Lucas Qin <87470079+lucas-project@users.noreply.github.com>
2025-07-08 22:15:47 +1000

    Added meta box

--- wp-content/plugins/custom_songs_type/songs-post-type.php
+++ wp-content/plugins/custom_songs_type/songs-post-type.php
@@ -75,3 +75,113 @@ function create_genre_taxonomy() {
 }
 
 
+
+// Enqueue meta box styles
+add_action( 'admin_enqueue_scripts', 'song_meta_box_styles' );
+function song_meta_box_styles( $hook ) {
+    global $post;
+    
+    // Only load on song post type edit screens
+    if ( $hook == 'post-new.php' || $hook == 'post.php' ) {
+        if ( 'song' === get_post_type( $post ) ) {
+            wp_enqueue_style( 'song-meta-box-styles', plugins_url( 'css/meta-box-style.css', __FILE__ ) );
+        }
+    }
+}
+
+// Add meta box 
+add_action("add_meta_boxes", "add_song_meta_box");
+
+function add_song_meta_box() {
+    add_meta_box(
+        "song_meta_box", // Meta box ID
+        "Song Details", // Meta box title
+        "song_meta_box_callback", // Meta box callback function
+        "song", // The custom post type parameter 1
+        "normal", // Meta box location in the edit screen
+        "high" // Meta box priority
+    ); 
+}
+
+function song_meta_box_callback() { 
+    wp_nonce_field('song-nonce', 'meta-box-nonce');
+    global $post;
+    
+    // Get saved values
+    $author_name = get_post_meta($post->ID, 'author_name', true);
+    $release_date = get_post_meta($post->ID, 'release_date', true);
+    $duration = get_post_meta($post->ID, 'duration', true);
+    $featured_artists = get_post_meta($post->ID, 'featured_artists', true);
+    $producer = get_post_meta($post->ID, 'producer', true);
+    $lyrics = get_post_meta($post->ID, 'lyrics', true);
+    ?>
+    <table class="form-table">
+        <tr>
+            <th><label for="author_name">Author Name</label></th>
+            <td><input type="text" id="author_name" class="regular-text" name="author_name" value="<?php echo esc_attr($author_name); ?>" /></td>
+        </tr>
+        <tr>
+            <th><label for="release_date">Release Date</label></th>
+            <td><input type="date" id="release_date" class="regular-text" name="release_date" value="<?php echo esc_attr($release_date); ?>" /></td>
+        </tr>
+        <tr>
+            <th><label for="duration">Duration (MM:SS)</label></th>
+            <td><input type="text" id="duration" class="regular-text" name="duration" placeholder="e.g. 3:45" value="<?php echo esc_attr($duration); ?>" /></td>
+        </tr>
+        <tr>
+            <th><label for="featured_artists">Featured Artists</label></th>
+            <td><input type="text" id="featured_artists" class="regular-text" name="featured_artists" placeholder="Separate multiple artists with commas" value="<?php echo esc_attr($featured_artists); ?>" /></td>
+        </tr>
+        <tr>
+            <th><label for="producer">Producer</label></th>
+            <td><input type="text" id="producer" class="regular-text" name="producer" value="<?php echo esc_attr($producer); ?>" /></td>
+        </tr>
+        <tr>
+            <th><label for="lyrics">Lyrics</label></th>
+            <td><textarea id="lyrics" name="lyrics" rows="10" cols="50"><?php echo esc_textarea($lyrics); ?></textarea></td>
+        </tr>
+    </table>
+    <?php  
+}
+
+// Save meta box data
+add_action('save_post', 'author_save_postdata');
+function author_save_postdata($post_id) {
+    // If this is an autosave, our form has not been submitted
+    if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) {
+        return $post_id;
+    }
+
+    // Verify nonce
+    if (!isset($_POST['meta-box-nonce']) || !wp_verify_nonce($_POST['meta-box-nonce'], 'song-nonce')) {
+        return $post_id;
+    }
+
+    // Retrieve post type
+    if ('song' !== get_post_type($post_id)) {
+        return $post_id;
+    }
+
+    // Check the user's permissions
+    if (!current_user_can('edit_post', $post_id)) {
+        return $post_id;
+    }
+
+    /* OK, it is safe to save the data now. */
+    // Sanitize user input.
+    $mydata = sanitize_text_field( $_POST['author_name'] );
+    $mydata = sanitize_text_field( $_POST['release_date'] );
+    $mydata = sanitize_text_field( $_POST['duration'] );
+    $mydata = sanitize_text_field( $_POST['featured_artists'] );
+    $mydata = sanitize_text_field( $_POST['producer'] );
+    $mydata = sanitize_text_field( $_POST['lyrics'] );
+    
+    // Update the meta field in the database.
+    update_post_meta( $post_id, 'Author_Name', $_POST['Author_Name'] ); 
+    update_post_meta( $post_id, 'release_date', $_POST['release_date'] ); 
+    update_post_meta( $post_id, 'duration', $_POST['duration'] ); 
+    update_post_meta( $post_id, 'featured_artists', $_POST['featured_artists'] ); 
+    update_post_meta( $post_id, 'producer', $_POST['producer'] ); 
+    update_post_meta( $post_id, 'lyrics', $_POST['lyrics'] ); 
+    
+}
